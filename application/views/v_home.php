b871660b396e266ee739b47e696bc39ac2740555
joseminaya15 <jose.minayac15@gmail.com>
2018-06-25 12:53:36 -0500

    imagenes

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -25,19 +25,26 @@
     <link rel="stylesheet"    href="<?php echo RUTA_CSS?>style.css?v=<?php echo time();?>">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="#principal"><img src="<?php echo RUTA_IMG?>logo/hpe-intel.png"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a class="nav-link" href="#home">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#hotel">Hotel Information</a></li>
-                <li class="nav-item"><a class="nav-link" href="#agenda">Agenda</a></li>
-                <li class="nav-item"><a class="nav-link" href="#visa">Visa Information</a></li>
-                <li class="nav-item"><a class="nav-link" href="#register">Register</a></li>
-            </ul>
+    <nav class="navbar navbar-default">
+        <div class="js-container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#principal"><img src="<?php echo RUTA_IMG?>logo/hpe-intel.png"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="nav-item active"><a class="nav-link" href="#home">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#hotel">Hotel Information</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#agenda">Agenda</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#visa">Visa Information</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#register">Register</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
     <section id="principal">
